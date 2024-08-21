8d63dffc02b333c3ab0a1799c3fbea7e2d19f399
Alejandra <202100355d@utea.edu.pe>
2024-08-21 07:24:54 -0500

    formulario completo

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,24 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:app09_sharetpreference/widgets/my_drawer_widget.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
   @override
-  _HomePageState createState() => _HomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  bool _isDarkMode = false; // Variable para manejar el estado del modo oscuro
+  bool isDarkMode = false;
+  int gender = 1;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: _isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      theme: isDarkMode ? ThemeData.dark() : ThemeData.light(),
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Shared Preferences App"),
         ),
-        drawer: const MyDrawerWidget(),
+        drawer: MyDrawerWidget(),
         body: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
@@ -29,44 +28,78 @@ class _HomePageState extends State<HomePage> {
                 "Configuración General",
                 style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
               ),
-              const SizedBox(height: 12.0),
+              const SizedBox(
+                height: 12.0,
+              ),
               const TextField(
                 decoration: InputDecoration(
                   hintText: "Nombre completo",
                 ),
               ),
-              const SizedBox(height: 12.0),
+              const SizedBox(
+                height: 12.0,
+              ),
               const TextField(
                 decoration: InputDecoration(
                   hintText: "Dirección actual",
                 ),
               ),
-              const SizedBox(height: 12.0),
+              const SizedBox(
+                height: 12.0,
+              ),
               SwitchListTile(
-                value: _isDarkMode,
+                value: isDarkMode,
                 onChanged: (bool value) {
                   setState(() {
-                    _isDarkMode = value;
+                    isDarkMode = value;
                   });
                 },
                 title: const Text("Dark mode"),
               ),
-              const SizedBox(height: 12.0),
+              const SizedBox(
+                height: 12.0,
+              ),
               const Text(
                 "Gender",
                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
               RadioListTile(
                 value: 1,
-                groupValue: 1,
-                onChanged: (int? value) {},
+                groupValue: gender,
+                onChanged: (int? value) {
+                  setState(() {
+                    gender = value!;
+                  });
+                },
                 title: const Text("Male"),
               ),
               RadioListTile(
-                value: 1,
-                groupValue: 1,
-                onChanged: (int? value) {},
+                value: 2,
+                groupValue: gender,
+                onChanged: (int? value) {
+                  setState(() {
+                    gender = value!;
+                  });
+                },
                 title: const Text("Female"),
+              ),
+              const SizedBox(
+                height: 12.0,
+              ),
+              ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.save,
+                  color: Colors.white,
+                ),
+                label: const Text(
+                  "Save Data",
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  minimumSize: const Size(double.infinity, 50),
+                ),
               ),
             ],
           ),
