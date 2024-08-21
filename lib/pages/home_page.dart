d2f9e6c656da5e27cd5a37e5eb3939fa023b9408
Alejandra <202100355d@utea.edu.pe>
2024-08-21 07:07:28 -0500

    modo oscuroo

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,227 +1,77 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:app09_sharetpreference/widgets/my_drawer_widget.dart';
 
 class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
-  State<HomePage> createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  bool isDarkMode = false;
-
-  String gender = 'Male'; 
+  bool _isDarkMode = false; // Variable para manejar el estado del modo oscuro
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Home",
+    return MaterialApp(
+      theme: _isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Shared Preferences App"),
         ),
-      ),
-
-      /// pestaña
-      drawer: Drawer(
-        child: Column(
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: NetworkImage(
-                      "https://images.pexels.com/photos/13650913/pexels-photo-13650913.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
-                ),
-              ),
-              child: SizedBox(
-                width: double.infinity,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    CircleAvatar(
-                      radius: 30.0,
-                      backgroundColor: Colors.white,
-                      backgroundImage: NetworkImage(
-                          "https://images.pexels.com/photos/573299/pexels-photo-573299.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
-                    ),
-                    Text(
-                      "Yujin Flores",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      "Administrador",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.normal,
-                      ),
-                    ),
-                   ],
-
-                ),
-
-                              
-              ),
-
-              
-            ),
-
-            ListTile(
-              leading: Icon(
-                Icons.person
-              ),
-              title:Text(
-                "My profile"
-              ),
-            ),
-
-            ListTile(
-              leading: Icon(
-                Icons.file_copy
-              ),
-              title:Text(
-                "Portafolio"
-              ),
-            ),
-
-            ListTile(
-              leading: Icon(
-                Icons.lock
-              ),
-              title:Text(
-                "Change Password"
-              ),
-            ),
-
-            Divider(indent: 12, endIndent: 12,),
-
-            ListTile(
-              leading: Icon(
-                Icons.exit_to_app,
-                color:Colors.orange
-              ),
-              title:Text(
-                "Logout",
-                
-                
-              ),
-              
-            )
-          ],
-        ),
-
-        
-      ),
-
-/////
+        drawer: const MyDrawerWidget(),
         body: Padding(
-  padding: const EdgeInsets.all(16.0),
-  child: Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Text(
-        "Configuración general",
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 18.0,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      SizedBox(height: 16.0),
-      Text(
-        "Elvis",
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 14.0,
-          fontWeight: FontWeight.normal,
-        ),
-      ),
-
-      Divider(indent: 0, endIndent: 0,),
-
-      SizedBox(height: 16.0),
-
-      Text(
-        "Lopez",
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 14.0,
-          fontWeight: FontWeight.normal,
-        ),
-      ),
-      Divider(indent: 0, endIndent: 0,),
-
-      SizedBox(height: 16.0),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            "Dark Mode",
-            style: TextStyle(color: Colors.black,
-            fontSize: 14.0,
-            fontWeight: FontWeight.bold,),
-            
-          ),
-          Switch(
-            value: false, 
-            onChanged: (value) {
-              
-            },
-          ),
-        ],
-      ),
-      SizedBox(height: 16.0),
-      Text(
-        "Gender",
-        style: TextStyle(color: Colors.black),
-      ),
-      Row(
-              children: [
-                Radio<String>(
-                  value: 'Male',
-                  groupValue: gender,
-                  onChanged: (value) {
-                    setState(() {
-                      gender = value!;
-                    });
-                  },
-                ),
-                Text("Male"),
-                Radio<String>(
-                  value: 'Female',
-                  groupValue: gender,
-                  onChanged: (value) {
-                    setState(() {
-                      gender = value!;
-                    });
-                  },
-                ),
-                Text("Female"),
-              ],
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                // Lógica para guardar los datos
-               
-              },
-              child: Text("Save data",
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Configuración General",
+                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
               ),
-              
-            ),
-
-    ],
-  ),
-),
-
-
-
-
+              const SizedBox(height: 12.0),
+              const TextField(
+                decoration: InputDecoration(
+                  hintText: "Nombre completo",
+                ),
+              ),
+              const SizedBox(height: 12.0),
+              const TextField(
+                decoration: InputDecoration(
+                  hintText: "Dirección actual",
+                ),
+              ),
+              const SizedBox(height: 12.0),
+              SwitchListTile(
+                value: _isDarkMode,
+                onChanged: (bool value) {
+                  setState(() {
+                    _isDarkMode = value;
+                  });
+                },
+                title: const Text("Dark mode"),
+              ),
+              const SizedBox(height: 12.0),
+              const Text(
+                "Gender",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              RadioListTile(
+                value: 1,
+                groupValue: 1,
+                onChanged: (int? value) {},
+                title: const Text("Male"),
+              ),
+              RadioListTile(
+                value: 1,
+                groupValue: 1,
+                onChanged: (int? value) {},
+                title: const Text("Female"),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/widgets/my_drawer_widget.dart
+++ lib/widgets/my_drawer_widget.dart
@@ -1,10 +1,79 @@
 import 'package:flutter/material.dart';
 
-class MyDrawrWidget extends StatelessWidget {
-  const MyDrawrWidget({super.key});
+class MyDrawerWidget extends StatelessWidget {
+  const MyDrawerWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      /// Pestaña
+      drawer: Drawer(
+        child: Column(
+          children: [
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      "https://images.pexels.com/photos/13650913/pexels-photo-13650913.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const CircleAvatar(
+                    radius: 30.0,
+                    backgroundColor: Colors.white,
+                    backgroundImage: NetworkImage(
+                        "https://images.pexels.com/photos/573299/pexels-photo-573299.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+                  ),
+                  const SizedBox(height: 8.0),
+                  const Text(
+                    "Yujin Flores",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Text(
+                    "Administrador",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const ListTile(
+              leading: Icon(Icons.person),
+              title: Text("My profile"),
+            ),
+            const ListTile(
+              leading: Icon(Icons.file_copy),
+              title: Text("Portafolio"),
+            ),
+            const ListTile(
+              leading: Icon(Icons.lock),
+              title: Text("Change Password"),
+            ),
+            const Divider(indent: 12, endIndent: 12),
+            ListTile(
+              leading: const Icon(
+                Icons.exit_to_app,
+                color: Colors.orange,
+              ),
+              title: const Text("Logout"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
